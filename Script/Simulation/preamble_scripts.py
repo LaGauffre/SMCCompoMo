3bfb7decafce1252350e2817d49b4879b4077154
Pierre-O Goffard <pierre.olivier.goffard@gmail.com>
2022-01-28 19:01:07 +0100

    edits on notebooks

---
+++ Script/Simulation/preamble_scripts.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 10:33:59 2021
+
+@author: pierr
+"""
+import bayes_splicing as bs
+
+import pandas as pd
+import numpy as np
+import scipy.stats as st
+import scipy.special as sp
+import math as ma
+from scipy.optimize import minimize
+from joblib import Parallel, delayed
+import scipy as sc
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from time import time
+from IPython.display import display_html
+
+import datetime as dat
+
+import pickle
+import random
+random.seed(123)
+
